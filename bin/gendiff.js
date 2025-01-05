d24013879be19639bdd3b89cda0bebc012274dfd
GalinaBagram1987 <gaby_ev@mail.ru>
2025-01-05 19:59:47 +1000

    add f in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,10 +5,11 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-  .command("gendiff")
+  .command("gendiff <filepath1> <filepath2>")
   .description("Compares two configuration files and shows a difference.")
   .action(() => {
     console.log("Compares two configuration...");
   });
+program.option("-f, --format [type]", "output format");
 program.version("0.0.1");
 program.parse();
