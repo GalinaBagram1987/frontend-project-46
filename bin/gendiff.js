2c06109c3b704a62ac1f4c2168dd06fb2075f129
GalinaBagram1987 <gaby_ev@mail.ru>
2025-01-05 19:41:32 +1000

    add gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,29 +1,14 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import { Command } from "commander";
+
+const program = new Command();
 
 program
-  .name("gendiff")
-  .version("1.0.0")
-  .description("Compares two configuration files and show
-  .option("-v, --version", "output the version number")
-  .option("-h, --help", "output usage information");
-
-program.action(() => {
-  const options = program.opts(); // Получаем все установ
-  if (option.help) {
-    console.log(`gendiff -h
-
-            Usage: gendiff [options]
-          
-            Compares two configuration files and shows a 
-          
-            Options:
-              -V, --version        output the version num
-              -h, --help           output usage informati
-  }
-  if (option.version) {
-    console.log(`version('1.0.0')`);
-  }
-});
-program.parse(process.argv);
+  .command("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .action(() => {
+    console.log("Compares two configuration...");
+  });
+program.version("0.0.1");
+program.parse();
