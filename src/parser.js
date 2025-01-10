7437407bf7c7668f288dc762940ab5eeb17d013b
GalinaBagram1987 <gaby_ev@mail.ru>
2025-01-10 23:31:07 +1000

    add parser, getDiff

--- src/parser.js
+++ src/parser.js
@@ -4,10 +4,11 @@ import path from "path";
 import * as yaml from "js-yaml";
 import { getExtension, readFile } from "./utilits.js";
 
-const parse = (filePath) => {
+const getParseData = (filePath) => {
   const fileData = readFile(filePath);
   const fileType = getExtension(filePath);
   if (fileType === ".json") return JSON.parse(fileData);
   if (fileType === ".yml" || fileType === "yaml") return yaml.load(fileData);
   return `This extension (${fileType}) is not supported by the program`;
 };
+
--- src/utilits.js
+++ src/utilits.js
@@ -3,14 +3,50 @@ import fs from "fs";
 import path from "path";
 import * as yaml from "js-yaml";
 import { extname } from "path";
+import _ from "lodash";
 
-const formats = ["json", "yml"];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const readFile = (filePath) => {
-  const absolutePath = path.isAbsolute(filePath)
-    ? filePath
-    : path.resolve(process.cwd(), filePath);
-  return fs.readFileSync(absolutePath, "utf-8");
-};
+const getPath = (filename) => path.resolve(__dirname, "..", filename);
+
+export const readFile = (filePath) =>
+  fs.readFileSync(getPath(filePath), "utf-8");
 
 export const getExtension = (filePath) => extname(filePath);
+
+const obj1 = {
+  host: "hexlet.io",
+  timeout: 50,
+  proxy: "123.234.53.22",
+  follow: false,
+};
+
+const obj2 = {
+  timeout: 20,
+  verbose: true,
+  host: "hexlet.io",
+};
+
+export const getDifferent = (obj1, obj2) => {
+  if (Object.keys(obj1).length === 0 && Object.keys(obj2).length === 0)
+    return {};
+  const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
+  return keys.reduce((acc, key) => {
+    if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+      if (obj1[key] === obj2[key]) {
+        acc[key] = obj1[key];
+      } else {
+        acc[`- ${key}`] = obj1[key];
+        acc[`+ ${key}`] = obj2[key];
+      }
+    } else if (obj1.hasOwnProperty(key)) {
+      acc[`- ${key}`] = obj1[key];
+    } else if (obj2.hasOwnProperty(key)) {
+      acc[`+ ${key}`] = obj2[key];
+    }
+    return acc;
+  }, {});
+};
+
+console.log(getDifferent(obj1, obj2));
