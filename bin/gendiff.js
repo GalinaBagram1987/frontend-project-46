fffbc3ffacf90232050d10e916b69178c44c3c0e
GalinaBagram1987 <gaby_ev@mail.ru>
2025-01-15 19:57:16 +1000

    add change

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import { fileURLToPath } from "url";
-import fs from "fs";
-import path from "path";
-import { genDiff } from "../src/index.js";
-import {
-  getPath,
-  readFile,
-  getExtension,
-  getDifferent,
-} from "../src/utilits.js";
-import { getParseData } from "../src/parser.js";
+import { Command } from 'commander';
+// import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import path from 'path';
+import { genDiff } from '../src/index.js';
+// import {
+// getPath,
+// readFile,
+// getExtension,
+// getDifferent,
+// } from '../src/utilits.js';
+// import getParseData from '../src/parser.js';
 
 const program = new Command();
 
 program
-  .name("gendiff")
-  .description("Compares two configuration files and shows a difference.")
-  .option("-f, --format [type]", "output format")
-  .argument("<filepath1>")
-  .argument("<filepath2>")
-  .version("0.0.1")
-  .action((filepath1, filepath2, option) => {
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .version('0.0.1')
+  .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   });
 
