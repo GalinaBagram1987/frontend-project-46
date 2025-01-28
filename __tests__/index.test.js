a1f2e3dd965862ece37ce5ea821c5878b6c81823
GalinaBagram1987 <gaby_ev@mail.ru>
2025-01-28 22:01:23 +1000

    change test and json.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,17 +2,6 @@ import fs from 'fs';
 import genDiff from '../src/index.js';
 import { getPath, readFile } from '../src/utilits.js';
 
-test('genDiff, both files are empty', () => {
-  const filePath1 = getPath('__fixtures__/file1.json');
-  const filePath2 = getPath('__fixtures__/file1.json');
-  const file1 = readFile(filePath1);
-  const file2 = readFile(filePath2);
-  const expected = '';
-  if (file1.lendth === 0 && file2.lendth === 0) {
-    expect(genDiff(filePath1, filePath2)).toEqual(expected);
-  }
-});
-
 test('genDiff, if one of the files does not exist', () => {
   const checkPath = (filePath1, filePath2) => {
     if (!fs.existsSync(filePath1) || !fs.existsSync(filePath2)) {
