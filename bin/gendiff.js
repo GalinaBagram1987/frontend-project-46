26b01209fb5007f260c5bcf5739d7f4e8416b710
GalinaBagram1987 <gaby_ev@mail.ru>
2025-01-28 23:36:10 +1000

    del unnecessary files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-// import { fileURLToPath } from 'url';
-// import fs from 'fs';
-// import path from 'path';
 import genDiff from '../src/index.js';
-// import {
-// getPath,
-// readFile,
-// getExtension,
-// getDifferent,
-// } from '../src/utilits.js';
-// import getParseData from '../src/parser.js';
 
 const program = new Command();
 
